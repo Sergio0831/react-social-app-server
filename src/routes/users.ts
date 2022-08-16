5340d6bdcbfb869d25c17a790867e384b257b93f
Sergio0831 <sergejs.ivcenko@gmail.com>
2022-08-16 08:47:37 +0100

    Implemented register and login

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -2,6 +2,6 @@ import express from 'express';
 
 export const usersRoute = express.Router();
 
-usersRoute.get('/', (req, res) => {
+usersRoute.get('/', async (req, res) => {
   res.send("hey it's user router");
 });
